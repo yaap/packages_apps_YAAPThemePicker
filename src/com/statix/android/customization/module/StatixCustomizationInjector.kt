a2b35db45bfb828fab14229469c3a82a29a21678
Anay Wadhera <awadhera@berkeley.edu>
2022-01-06 03:03:28 +0000

    ThemePicker: implement coloring in wallpaper preview fragment
    Change-Id: I78206bbe6b96529975b5cefd0a3a2ae55623c505
    Reviewed-on: https://review.statixos.com/c/android_packages_apps_ThemePicker/+/6613
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Reviewed-on: https://review.statixos.com/c/android_packages_apps_ThemePicker/+/7123

--- src/com/statix/android/customization/module/StatixCustomizationInjector.kt
+++ src/com/statix/android/customization/module/StatixCustomizationInjector.kt
@@ -1,5 +1,23 @@
 package com.statix.android.customization.module
 
+import android.content.Context
+
+import androidx.fragment.app.Fragment
+
+import com.android.wallpaper.model.WallpaperInfo
+import com.android.wallpaper.picker.MonetPreviewFragment
+
 import com.android.customization.module.DefaultCustomizationInjector
 
-public class StatixCustomizationInjector : DefaultCustomizationInjector()
+public class StatixCustomizationInjector : DefaultCustomizationInjector() {
+
+    override fun getPreviewFragment(
+            context: Context,
+            wallpaperInfo: WallpaperInfo,
+            mode: Int,
+            viewAsHome: Boolean,
+            viewFullScreen: Boolean,
+            testingModeEnabled: Boolean): Fragment {
+        return MonetPreviewFragment.newInstance(wallpaperInfo, mode, viewAsHome, viewFullScreen, testingModeEnabled);
+    }
+}
